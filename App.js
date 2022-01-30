d47813f4be619347195cdb8858ebc61603f9d0f8
신현수 <sinhyeonsu@MacBook-Pro.local>
2022-01-30 16:23:59 +0900

    card finish

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
-import { Animated, PanResponder, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, PanResponder, Text, View } from "react-native";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
+import icons from "./icons";
 
 const Container = styled.View`
   flex: 1;
@@ -17,12 +18,35 @@ const Card = styled(Animated.createAnimatedComponent(View))`
   align-items: center;
   border-radius: 12px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+  position: absolute;
+`;
+const Btn = styled.TouchableOpacity`
+  margin: 0px 5px;
+`;
+const BtnContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+const CardContainer = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default function App() {
   //Values
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-280, 280],
+    outputRange: ["-15deg", "15deg"],
+  });
+  const secondScale = position.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.5, 1],
+    extrapolate: "clamp",
+  });
+
   //Animmatinos
   const onPressOut = Animated.spring(scale, {
     toValue: 1,
@@ -36,6 +60,22 @@ export default function App() {
     toValue: 0,
     useNativeDriver: true,
   });
+  const goLeft = Animated.spring(position, {
+    toValue: -500,
+    useNativeDriver: true,
+    tension: 5,
+    restDisplacementThreshold: 100,
+    restSpeedThreshold: 100,
+  });
+  // restDisplacementThreshold 거리 임계값 스프링 일때 해당 남은 애니메이션 거리값이 임계치에 다다르면 스킵..
+  // restSpeedThreshold 속도 임계값 이 속도가 되었으때 애니메이션 끝내라...
+  const goRight = Animated.spring(position, {
+    toValue: 500,
+    useNativeDriver: true,
+    tension: 5,
+    restDisplacementThreshold: 100,
+    restSpeedThreshold: 100,
+  });
 
   //Pan Responders
   const panResponder = useRef(
@@ -45,22 +85,57 @@ export default function App() {
         position.setValue(dx);
       },
       onPanResponderGrant: () => onPressIn.start(),
-      onPanResponderRelease: () => {
-        Animated.parallel([onPressOut, goCenter]).start();
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -200) {
+          goLeft.start(onDismiss);
+        } else if (dx > 200) {
+          goRight.start(onDismiss);
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     })
   ).current;
-
+  const closePress = () => {
+    goLeft.start(onDismiss);
+  };
+  const checkPress = () => {
+    goRight.start(onDismiss);
+  };
+  //state
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    scale.setValue(1);
+    position.setValue(0);
+    setIndex((prev) => prev + 1);
+  };
   return (
     <Container>
-      <Card
-        {...panResponder.panHandlers}
-        style={{
-          transform: [{ scale }, { translateX: position }],
-        }}
-      >
-        <Ionicons name="pizza" color="#192a56" size={98} />
-      </Card>
+      <CardContainer>
+        <Card style={{ transform: [{ scale: secondScale }] }}>
+          <Ionicons name={icons[index + 1]} color="#192a56" size={98} />
+        </Card>
+        <Card
+          {...panResponder.panHandlers}
+          style={{
+            transform: [
+              { scale },
+              { translateX: position },
+              { rotateZ: rotation },
+            ],
+          }}
+        >
+          <Ionicons name={icons[index]} color="#192a56" size={98} />
+        </Card>
+      </CardContainer>
+      <BtnContainer>
+        <Btn onPress={closePress}>
+          <Ionicons name="close-circle" color="white" size={58} />
+        </Btn>
+        <Btn onPress={checkPress}>
+          <Ionicons name="checkmark-circle" color="white" size={58} />
+        </Btn>
+      </BtnContainer>
     </Container>
   );
 }
